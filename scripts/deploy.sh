c04a637fd57239972a1e647f16e8f4672827e8f8
Matt Jones <gitcode@magisa.org>
2020-06-10 12:09:16 -0800

    Move the deployed files into the published location.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,6 +6,12 @@
 # DEPLOY_USER: username of the user to be logged in on the deployment host
 # DEPLOY_HOST: the target host for deployment
 # DEPLOY_DIRECTORY: the directory for files to be copied to on the deployment host
+# PUBLISH_DIRECTORY: the directory where final files are published after they have been deployed to the host
+
+# Note that the rsync moves the files to the DEPLOY_DIRECTORY, but that in a separate step we move the files
+# into the PUBLISH_DIRECTORY to ensure file consistency
 
 ssh -p22 $DEPLOY_USER@$DEPLOY_HOST "mkdir -p $DEPLOY_DIRECTORY" && \
-    rsync -rav -e ssh ./docs/ $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_DIRECTORY
+    rsync -rav -e ssh ./docs/ $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_DIRECTORY && \
+    ssh -p22 $DEPLOY_USER@$DEPLOY_HOST "mv $DEPLOY_DIRECTORY $PUBLISH_DIRECTORY"
+
