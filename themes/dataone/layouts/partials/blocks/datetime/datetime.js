ef5b70293b3c2ef83c2a35ac1e0783101ffbe1a9
Robyn Thiessen-Bock <robyn.thiessenbock@gmail.com>
2021-03-23 18:50:26 -0400

    Fix the datetime block
    - Remove dependency on external API which will soon be deprecated, use hardcoded list of timezones instead

--- themes/dataone/layouts/partials/blocks/datetime/datetime.js
+++ themes/dataone/layouts/partials/blocks/datetime/datetime.js
@@ -1,26 +1,48 @@
-/**
- * toggleTimezoneInput - Handler function for when the "edit timezone" button is
- * clicked.
- * @param  {HTMLElement} button The "edit timezone" button
- * 
- */ 
-var toggleTimezoneInput = function (button) {
-  if(button){
-    var open = JSON.parse(button.getAttribute("aria-expanded"));
-    // get the input element, a sibling of the button
-    inputEl = button.parentNode.getElementsByClassName("datetime__tz-input")[0];
-    d1Utilities.toggleElement(button, inputEl, open);
-    var inputElinput = inputEl.querySelector("input");
-    if(inputElinput){
-      inputElinput.focus();
-      inputElinput.value = "";
-      // inputEl.clear();
-    }
-  }
-  
-};
+// {{- $site := . -}}
+// {{- $bnBase := "datetime" -}}
+// {{- $p := $site.Params.prefix -}}
+// {{- $bn := print $p $bnBase -}}
+// {{- $blockDir:=  $site.Params.blockDir -}}
+// {{- $timezonesPath := path.Join $blockDir (print $bnBase "/timezones.json") -}}
+// {{ $timezoneString := readFile $timezonesPath }}
 
-(function() {
+/**
+ * {{ $bn }} - Functions for the date time elements
+ */ 
+(function( {{ $bn }}, undefined ) {
+
+  // The block's JS
+  var block = {{ $bn }};
+  var blockName = "{{ $bn }}";
+
+  // The timezone data
+  const timezones = {{ $timezoneString }};
+
+  const inputSelector = "." + blockName + "__tz-input";
+
+  // Get the datetime (<time>) elements. Each contains the display time,
+  // the timezone, as well as the input element.
+  block.timeEls = document.getElementsByClassName(blockName);
+  // Get the user's time zone, default to DataONE location
+  block.userTZ = moment.tz.guess() || "America/Los_Angeles";
+
+  var localizeTimes = function(){
+    if(block.timeEls){
+      for (var i = 0; i < block.timeEls.length; i++) {
+        
+        var timeEl = block.timeEls[i];
+        var localize = timeEl.classList.contains(blockName + "--localize");
+        
+        // Update the displayed datetime and timezone to the user's local time.
+        // if timeEl has class "datetime--localized"
+        if(localize){
+          changeTimezone(timeEl, block.userTZ);
+        }
+        
+      }
+    }
+    
+  }
 
   /**
    * var changeTimezone - Converts the displayed time in an HTML <time> element to a specified timezone
@@ -42,9 +64,9 @@ var toggleTimezoneInput = function (button) {
 
       if(timezone && timeEl){
         // Get the displayDate element
-        var displayDateEl = timeEl.getElementsByClassName("datetime__displaydate")[0];
+        var displayDateEl = timeEl.getElementsByClassName(blockName + "__displaydate")[0];
         // Get the timezone element
-        var timezoneEl = timeEl.getElementsByClassName("datetime__timezone")[0];
+        var timezoneEl = timeEl.getElementsByClassName(blockName + "__timezone")[0];
         // Get the display format
         var format = timeEl.dataset.format;
         // Get the ISO time stamp from the datetime attribute
@@ -74,155 +96,91 @@ var toggleTimezoneInput = function (button) {
     }
   }
 
-  /**
-   * getTimezone - Find the Internet Assigned Numbers Authority timezone associated with specific geo coordinates.
-   *  
-   * @param  {number} lat                             Latitude
-   * @param  {number} lng                             Longitude
-   * @param  {type} username = "marvelous_envelope"   user name for the geonames.org API
-   * @param  {function} callback                      A function to call after the timezone is retrieved successfully
-   * @return {string|boolean}                         Internet Assigned Numbers Authority timezone, or false if there was an error
-   */ 
-  var getTZFromCoords = function (lat, lng, callback, username = "marvelous_envelope"){
-    var geonamesEndpoint = "http://api.geonames.org/timezoneJSON?lat=" + lat + "&lng=" + lng + "&username=" + username;
-    var request = new XMLHttpRequest();
-    request.open('GET', geonamesEndpoint, true);
-    request.onload = function() {
-      var zone = JSON.parse(this.response);
-      if (request.status >= 200 && request.status < 400) {
-        callback(zone.timezoneId);
-      } else {
-        console.log('error getting timezone from coordinates, response from geonames: ' + response);
-        return false;
-      }
-    }
-    request.send();
-  };
-
-  // Set up Algolia search
-  var client = algoliasearch('QOXEJBQETE', 'bbe8439854be88831fb17d73831df877');
-  var index = client.initIndex('timezones');
-
-  // create the first autocomplete.js dataset: timezones
-  var timezones = {
-    source: autocomplete.sources.hits(index, {hitsPerPage: 2}),
-    displayKey: 'name',
-    templates: {
-      header: '<div class="tz-input-header">Timezones</div>',
-        suggestion({_highlightResult}) {
-          return `<span class="ap-suggestion-icon" style="white-space:normal;">
-          <svg width="20" height="21" xmlns="http://www.w3.org/2000/svg"><g fill="currentcolor" fill-rule="evenodd"><path d="M16.8.5v7.4a7.5 7.5 0 00-5.4 0v-3L5.9 3.2v12.6L.5 18V3.4L5.9 1l5.5 2.1L16.8.5z"/><path d="M14 9.4a5.5 5.5 0 110 11 5.5 5.5 0 010-11zm0 1.8a3.6 3.6 0 100 7.3 3.6 3.6 0 000-7.3z"/><path d="M13 15l1.5-2.7 1.2.7-1 1.7 1.2 1.2-1 1z"/></g></svg>
-          </span>
-          <span>${_highlightResult.name.value}</span>`;
-        }
-    }
-  };
-
-  // create the second dataset: places
-  // we automatically inject the default CSS
-  // all the places.js options are available
-  var placesDataset = placesAutocompleteDataset({
-    appId: 'plOAL9PGY18J',
-    apiKey: '7949704c8499dcba871219e38890255a',
-    algoliasearch: algoliasearch,
-    templates: {
-      header: '<div class="tz-input-header">Cities</div>'
-    },
-    hitsPerPage: 3
-  });
-
-  // Get the datetime (<time>) elements. Each contains the display time,
-  // the timzone, as well as the algolia search input element.
-  var timeEls = document.getElementsByClassName("datetime");
-  // Get the user's time zone, default to DataONE location
-  var userTZ = moment.tz.guess() || "America/Los_Angeles";
-  
-  
-  for (var i = 0; i < timeEls.length; i++) {
-    
-    var timeEl = timeEls[i];
-    var inputEl = timeEl.getElementsByClassName("datetime__tz-input")[0];
-    var change_timezone = timeEl.classList.contains("datetime--change-timezone");
-    var localize = timeEl.classList.contains("datetime--localize");
-    
-    // Update the displayed datetime and timezone to the user's local time.
-    // if timeEl has class "datetime--localized"
-    if(localize){
-      changeTimezone(timeEl, userTZ);
-    }
-    
-    // Allow user to select a different timezone if timeEl has class "datetime--localized"
-    if(change_timezone){
-      // Initiate the Algolia search
-      var autocompleteInstance = autocomplete(inputEl, {
-        hint: false,
-        debug: true,
-        cssClasses: {
-          root: 'datetime__tz-input',
-          prefix: 'tz-input'
-        }
-      }, [
-        timezones,
-        placesDataset
-      ]);
-
-      var autocompleteChangeEvents = ['selected', 'autocompleted'];
-      
-      /**    
-       * handleAutocomplete - What to do when a search option is selected or completed
-       *      
-       * @param  {event}          event         The autocomplete event   
-       * @param  {object}         suggestion    The found search item
-       * @param  {string|number}  datasetName   The ID used for the dataset (places for locations or 1 for timezones)  
-       */     
-      var handleAutocomplete = function(event, suggestion, datasetName) {
-        
-        console.log("handle auto comp this:");
-        console.log(this);
-
-        // make sure we have the correct tiime element (TODO)
-        var timeEl = this.parentNode.parentNode;
-        
-        if(datasetName == "places"){
-          getTZFromCoords(suggestion.latlng.lat, suggestion.latlng.lng, function(newTimezone){
-            changeTimezone(timeEl, newTimezone);
+  var initializeAutocomplete = function(){
+    // The autoComplete.js Engine instance creator
+    const autoCompleteJS = new autoComplete({
+      selector: inputSelector,
+      name: "timezones",
+      data: {
+        src: timezones,
+        key: ["name"],
+        results: (list) => {
+          // Filter duplicates
+          const filteredResults = Array.from(
+            new Set(list.map((value) => value.match))
+          ).map((tz) => {
+            return list.find((value) => value.match === tz);
           });
-        } else {
-          if(suggestion.name){
-            changeTimezone(timeEl, suggestion.name);
-          }
+          return filteredResults;
         }
-        
-        // Clear and hide the input again
-        this.blur();
-        
-      };
-      
-
-      // Update the datetime and timezone when user selects from search
-      autocompleteChangeEvents.forEach(function(eventName) {
-        
-        autocompleteInstance.off('autocomplete:'+ eventName, handleAutocomplete);
-        autocompleteInstance.on('autocomplete:'+ eventName, handleAutocomplete);
-        
-      });
-      
-      /**    
-       * handleFocusout - What to do when the search input loses focus
-       */
-      var handleFocusout = function(){
-        var inputEl = this;
-        var timeEl = this.parentNode.parentNode;
-        var button = timeEl.querySelector("button");
-        inputEl.value = ""
-        toggleTimezoneInput(button);
+      },
+      trigger: {
+        event: ["input", "focus"]
+      },
+      searchEngine: "strict",
+      highlight: true,
+      maxResults: 7,
+      resultItem: {
+        content: (data, element) => {
+          // Modify Results Item Style
+          element.style = "display: flex; flex-direction: column;";
+          // Modify Results Item Content
+          element.innerHTML = `<span style="text-overflow: ellipsis; white-space: nowrap; overflow: hidden;">
+            ${data.match}</span>
+            <span style="margin-top: 4px; display: flex; align-items: center; font-size: 13px; font-weight: 300; text-transform: uppercase; color: rgba(0,0,0,.4);">
+          ${data.value.value}</span>`;
+        }
+      },
+      noResults: (dataFeedback, generateList) => {
+        // Generate autoComplete List
+        generateList(autoCompleteJS, dataFeedback, dataFeedback.results);
+        // No Results List Item
+        const result = document.createElement("li");
+        result.setAttribute("class", "no_result");
+        result.setAttribute("tabindex", "1");
+        result.innerHTML = `<span style="display: flex; align-items: center; font-weight: 100; color: rgba(0,0,0,.2);">Found No Results for "${dataFeedback.query}"</span>`;
+        document
+          .querySelector(`#${autoCompleteJS.resultsList.idName}`)
+          .appendChild(result);
+      },
+      onSelection: (feedback) => {
+        // Change the time
+        var timeEl = feedback.event.target.closest('time');
+        var newTimezone = feedback.selection.value.value;
+        changeTimezone(timeEl, newTimezone);
+        // Blur, empty, and hide the input
+        document.querySelector("#autoComplete").blur();
+        document.querySelector("#autoComplete").value = "";
+        block.toggleTimezoneInput(document.querySelector("#autoComplete ~ button"));
       }
-      
-      inputEl.removeEventListener("focusout", handleFocusout);
-      inputEl.addEventListener("focusout", handleFocusout);
-      
-    } // --- end if(change_timezone)--- 
-    
+    });
   }
 
-})();
+  /**
+   * toggleTimezoneInput - Handler function for when the "edit timezone" button is
+   * clicked.
+   * @param  {HTMLElement} button The "edit timezone" button
+   * 
+   */ 
+  block.toggleTimezoneInput = function (button) {
+    if(button){
+      var open = JSON.parse(button.getAttribute("aria-expanded"));
+      // get the input element, a sibling of the button
+      inputEl = button.parentNode.getElementsByClassName(blockName + "__tz-input")[0];
+      d1Utilities.toggleElement(button, inputEl, open);
+      block.inputElinput = inputEl.querySelector("input");
+      if(block.inputElinput){
+        block.inputElinput.focus();
+        block.inputElinput.value = "";
+      }
+    }
+  };
+
+  localizeTimes();
+
+  if(document.querySelector(inputSelector)){
+    initializeAutocomplete();
+  }
+
+}( window.{{ $bn }} = window.{{ $bn }} || {} ));
+
